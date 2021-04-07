1fcf2157f6aaaf725fa5838b50237aec8762033c
Amar Patel <amar.p86@gmail.com>
2021-04-07 12:33:10 -0400

    adding home content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,18 @@
-function Home(props){
-    return (
-        <div className ="home">
-            Home Goes Here
+import image from "../images/home-page-image.jpeg";
+import "./Home.css";
+
+function Home(props) {
+  return (
+    <div className="home">
+      <div className="home-image-area">
+        <img className="home-image" src={image} alt="home-banner" />
+        <div className="home-image-text">
+          <h2>Amar Patel</h2>
+          <h4>Software Engineer</h4>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
