d80e85b3d2afe109950e253950b2abe69750f28b
Amar Patel <amar.p86@gmail.com>
2021-04-07 17:21:48 -0400

    more styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,9 +4,9 @@ import "./Home.css";
 function Home(props) {
   return (
     <div className="home">
-      <div className="home-image-area">
-        <img className="home-image" src={image} alt="home-banner" />
-      </div>
+      <h1>Software Engineer | Full Stack Developer</h1>
+      <img className="home-image" src={image} alt="home-banner" />
+
       <div className="home-image-text">
         <h2>Amar Patel</h2>
         <h4>Software Engineer</h4>
