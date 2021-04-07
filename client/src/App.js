d12b6218b4bf6cd43603c2f9f60751c783287807
Amar Patel <amar.p86@gmail.com>
2021-04-07 12:41:49 -0400

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./components/Home"
 import Nav from "./components/layout/Nav"
 import Footer from "./components/layout/Footer"
-import {Route, Link} from "react-router-dom"
+import {Route} from "react-router-dom"
 import Projects from "./screens/Projects"
 
 function App() {
