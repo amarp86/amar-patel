c4c93677cb2d1acfe79d7fae5e17baa49dce4a20
Amar Patel <amar.p86@gmail.com>
2021-04-08 14:10:07 -0400

    form works

--- client/src/components/layout/Nav.jsx
+++ client/src/components/layout/Nav.jsx
@@ -9,6 +9,12 @@ function Nav(props) {
       <div className="menu-links">
         <Link to="/projects">Recent Projects</Link>
         <Link to="/contact">Contact Me</Link>
+        <a
+          href="https://1drv.ms/w/s!Ah5HXdHVlST5kRK2P5m94N1cnV2o"
+          target="blank"
+        >
+          Resume
+        </a>
       </div>
     </div>
   );
